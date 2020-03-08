a32abf8a716f7941a16acfcbec57819e0313dc61
ここにユーザー名 <>
2020-03-08 14:10:32 +0900

    管理者側お知らせview,controller作成

--- app/controllers/admin/notices_controller.rb
+++ app/controllers/admin/notices_controller.rb
@@ -1,26 +1,48 @@
 class Admin::NoticesController < ApplicationController
 	def index
+		@notices = Notice.all
 	end
 
 	def new
+		@notice = Notice.new
 	end
 
 	def create
+		@notice = Notice.new(notice_params)
+		if @notice.save #入力されたデータをdbに保存する。
+	      redirect_to admin_notices_path
+	    else
+	      render 'index'
+	    end
+	end
+
+	def show
+		@notice = Notice.find(params[:id])
 	end
 
 	def edit
+		@notice = Notice.find(params[:id])
 	end
 
 	def update
+		@notice = Notice.find(params[:id])
+		if @notice.update(notice_params)
+			redirect_to admin_notice_path(@notice.id)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@notice = Notice.find(params[:id])
+		@notice.destroy
+    	redirect_to admin_notices_path
 	end
 
 	private
 
 	def notice_params
-	    params.require(:notice).permit(:image_id,:title,:content)
+	    params.require(:notice).permit(:image,:title,:content)
 	end
 
 
