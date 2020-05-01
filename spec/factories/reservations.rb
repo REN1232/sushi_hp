638e6aabe4ecc371644faa17182e4668efad88e6
ここにユーザー名 <>
2020-05-01 15:42:35 +0900

    FactoryBot予約人数変更

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :reservation do
   	customer_name {Faker::Name.name}
-  	people {Faker::Number.between(from: 1, to: 10)}
+  	people {Faker::Number.between(from: 1, to: 2)}
   	reservation_time {"11:00"}
   	reservation_day {Faker::Date.forward(days: 100)}
   	phone_number {Faker::Number.number(digits: 10)}
