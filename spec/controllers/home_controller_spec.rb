491cb1ccf43cedf2f19a13fd7bf921a6954253e9
ここにユーザー名 <>
2020-05-01 17:52:03 +0900

    テスト設計homeコントローラー完成

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe 'アバウトページ' do
+    context "アバウトページが正しく表示される" do
+      before do
+        get :about
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'indexテンプレートで表示されること' do
+        expect(response).to render_template :about
+      end
+    end
+  end
+  describe 'トップページ' do
+    context "トップページが正しく表示される" do
+      before:each do
+        get :top
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'assigns top @notices' do
+        expect(assigns(:home)).to eq @notices
+      end
+      it 'topテンプレートで表示されること' do
+        expect(response).to render_template :top
+      end
+    end
+  end
+end
