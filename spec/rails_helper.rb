be847c3699fcf280d5815583eb7c4fa7fc213779
ここにユーザー名 <>
2020-05-04 15:32:03 +0900

    テストでの管理者ログイン設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,5 @@ RSpec.configure do |config|
     config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
     config.include ::Rails::Controller::Testing::Integration, type: type
   end
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
