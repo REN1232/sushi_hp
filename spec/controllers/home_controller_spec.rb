d2d9db65a281b46f443b8e5193cfface5ba44512
ここにユーザー名 <>
2020-05-07 15:11:15 +0900

    [Rspec]home_controller修正

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,19 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  describe 'アバウトページ' do
-    context "アバウトページが正しく表示される" do
-      before do
-        get :about
-      end
-      it 'リクエストは200 OKとなること' do
-        expect(response.status).to eq 200
-      end
-      it 'aboutテンプレートで表示されること' do
-        expect(response).to render_template :about
-      end
-    end
-  end
   describe 'トップページ' do
     context "トップページが正しく表示される" do
       before:each do
@@ -27,6 +14,58 @@ RSpec.describe HomeController, type: :controller do
       end
       it 'topテンプレートで表示されること' do
         expect(response).to render_template :top
+      end
+    end
+  end
+  describe 'アバウトページ' do
+    context "アバウトページが正しく表示される" do
+      before do
+        get :about
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'aboutテンプレートで表示されること' do
+        expect(response).to render_template :about
+      end
+    end
+  end
+  describe '店舗ページ' do
+    context "店舗ページが正しく表示される" do
+      before do
+        get :store
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'storeテンプレートで表示されること' do
+        expect(response).to render_template :store
+      end
+    end
+  end
+  describe 'メニューページ' do
+    context "メニューページが正しく表示される" do
+      before do
+        get :menus
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'menusテンプレートで表示されること' do
+        expect(response).to render_template :menus
+      end
+    end
+  end
+  describe 'こだわりページ' do
+    context "こだわりページが正しく表示される" do
+      before do
+        get :kodawari
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'kodawariテンプレートで表示されること' do
+        expect(response).to render_template :kodawari
       end
     end
   end
