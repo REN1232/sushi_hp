1ccaa27db40655272ae9a6f53100a294d559f0f2
ここにユーザー名 <>
2020-05-07 15:00:25 +0900

    Homeコントローラー修正

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,18 +1,7 @@
 class Admin::HomeController < ApplicationController
 	before_action :authenticate_admin!
-	def top
-	end
 
 	def about
-	end
-
-	def kodawari
-	end
-
-	def menus
-	end
-
-	def store
 	end
 
 end
